e8d63c93ab14b8bfc424b08c06bc888a8cc2c67a
avecenob <aryabasc@gmail.com>
2025-09-26 14:49:29 +0700

    update category card

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -5,12 +5,14 @@ interface Props {
 function CategoryCard({ name }: Props) {
 	return (
 		<div className="col">
-			<div
-				className="border rounded d-flex flex-column align-items-center p-3 h-100"
-				style={{ maxWidth: '150px', minHeight: '100px' }}
-			>
-				<p>{name}</p>
-			</div>
+			<a href="">
+				<div
+					className="border rounded d-flex flex-column align-items-center p-3 h-100"
+					style={{ maxWidth: '150px', minHeight: '100px' }}
+				>
+					<p>{name}</p>
+				</div>
+			</a>
 		</div>
 	);
 }
